rustic-rs/rustic_core

commit ff13befa039308512331901952f91f884e782bbe

feat: Add read-data-subset to CheckOptions; allow to check given trees (#262)
Adds `Repository::check_with_trees` which allows to do tree and data
check only for the given trees.

Moreover, `CheckOptions` now have the `read_data_subset` option which
allows to read only parts of the pack files.

Also warming up of packfiles in case of hot/cold respository has been
added.

closes https://github.com/rustic-rs/rustic/issues/251

---------

Co-authored-by: simonsan <14062932+simonsan@users.noreply.github.com>

crates/core/src/commands/check.rs
@@ -1,18 +1,23 @@
 //! `check` subcommand
-use std::collections::HashMap;
+use std::{
+    collections::{BTreeSet, HashMap},
+    str::FromStr,
+};
 
 use bytes::Bytes;
+use bytesize::ByteSize;
 use derive_setters::Setters;
-use itertools::Itertools;
 use log::{debug, error, warn};
-use rayon::prelude::{IntoParallelIterator, ParallelBridge, ParallelIterator};
+use rand::{prelude::SliceRandom, thread_rng, Rng};
+use rayon::prelude::{IntoParallelIterator, ParallelIterator};
 use zstd::stream::decode_all;
 
 use crate::{
     backend::{cache::Cache, decrypt::DecryptReadBackend, node::NodeType, FileType, ReadBackend},
     blob::{tree::TreeStreamerOnce, BlobId, BlobType},
     crypto::hasher::hash,
-    error::{RusticErrorKind, RusticResult},
+    error::{CommandErrorKind, RusticErrorKind, RusticResult},
+    id::Id,
     index::{
         binarysorted::{IndexCollector, IndexType},
         GlobalIndex, ReadGlobalIndex,
@@ -20,10 +25,96 @@ use crate::{
     progress::{Progress, ProgressBars},
     repofile::{
         packfile::PackId, IndexFile, IndexPack, PackHeader, PackHeaderLength, PackHeaderRef,
-        SnapshotFile,
     },
     repository::{Open, Repository},
+    TreeId,
 };
+
+#[derive(Clone, Copy, Debug, Default)]
+/// Options to specify which subset of packs will be read
+pub enum ReadSubsetOption {
+    #[default]
+    /// Read all pack files
+    All,
+    /// Read a random subset of pack files with (approximately) the given percentage of total size
+    Percentage(f64),
+    /// Read a random subset of pack files with (approximately) the given size
+    Size(u64),
+    /// Read a subset of packfiles based on Ids: Using (1,n) .. (n,n) in separate runs will cover all pack files
+    IdSubSet((u32, u32)),
+}
+
+impl ReadSubsetOption {
+    fn apply(self, packs: impl IntoIterator<Item = IndexPack>) -> Vec<IndexPack> {
+        self.apply_with_rng(packs, &mut thread_rng())
+    }
+
+    fn apply_with_rng(
+        self,
+        packs: impl IntoIterator<Item = IndexPack>,
+        rng: &mut impl Rng,
+    ) -> Vec<IndexPack> {
+        fn id_matches_n_m(id: &Id, n: u32, m: u32) -> bool {
+            id.as_u32() % m == n % m
+        }
+
+        let mut total_size: u64 = 0;
+        let mut packs: Vec<_> = packs
+            .into_iter()
+            .inspect(|p| total_size += u64::from(p.pack_size()))
+            .collect();
+
+        // Apply read-subset option
+        if let Some(mut size) = match self {
+            Self::All => None,
+            // we need some casts to compute percentage...
+            #[allow(clippy::cast_possible_truncation)]
+            #[allow(clippy::cast_precision_loss)]
+            #[allow(clippy::cast_sign_loss)]
+            Self::Percentage(p) => Some((total_size as f64 * p / 100.0) as u64),
+            Self::Size(s) => Some(s),
+            Self::IdSubSet((n, m)) => {
+                packs.retain(|p| id_matches_n_m(&p.id, n, m));
+                None
+            }
+        } {
+            // random subset of given size is required
+            packs.shuffle(rng);
+            packs.retain(|p| {
+                let p_size = u64::from(p.pack_size());
+                if size > p_size {
+                    size = size.saturating_sub(p_size);
+                    true
+                } else {
+                    false
+                }
+            });
+        }
+        packs
+    }
+}
+
+impl FromStr for ReadSubsetOption {
+    type Err = CommandErrorKind;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let result = if s == "all" {
+            Self::All
+        } else if let Some(p) = s.strip_suffix('%') {
+            // try to read percentage
+            Self::Percentage(p.parse()?)
+        } else if let Some((n, m)) = s.split_once('/') {
+            // try to read n/m
+            Self::IdSubSet((n.parse()?, m.parse()?))
+        } else {
+            Self::Size(
+                ByteSize::from_str(s)
+                    .map_err(CommandErrorKind::FromByteSizeParser)?
+                    .as_u64(),
+            )
+        };
+        Ok(result)
+    }
+}
 
 #[cfg_attr(feature = "clap", derive(clap::Parser))]
 #[derive(Clone, Copy, Debug, Default, Setters)]
@@ -34,9 +125,13 @@ pub struct CheckOptions {
     #[cfg_attr(feature = "clap", clap(long, conflicts_with = "no_cache"))]
     pub trust_cache: bool,
 
-    /// Read all data blobs
+    /// Also read and check pack files
     #[cfg_attr(feature = "clap", clap(long))]
     pub read_data: bool,
+
+    /// Read and check pack files
+    #[cfg_attr(feature = "clap", clap(long, default_value = "all"))]
+    pub read_data_subset: ReadSubsetOption,
 }
 
 impl CheckOptions {
@@ -54,7 +149,11 @@ impl CheckOptions {
     /// # Errors
     ///
     /// If the repository is corrupted
-    pub(crate) fn run<P: ProgressBars, S: Open>(self, repo: &Repository<P, S>) -> RusticResult<()> {
+    pub(crate) fn run<P: ProgressBars, S: Open>(
+        self,
+        repo: &Repository<P, S>,
+        trees: Vec<TreeId>,
+    ) -> RusticResult<()> {
         let be = repo.dbe();
         let cache = repo.cache();
         let hot_be = &repo.be_hot;
@@ -105,37 +204,32 @@ impl CheckOptions {
             }
         }
 
-        let total_pack_size: u64 = index_collector
-            .data_packs()
-            .iter()
-            .map(|(_, size)| u64::from(*size))
-            .sum::<u64>()
-            + index_collector
-                .tree_packs()
-                .iter()
-                .map(|(_, size)| u64::from(*size))
-                .sum::<u64>();
-
         let index_be = GlobalIndex::new_from_index(index_collector.into_index());
 
-        check_snapshots(be, &index_be, pb)?;
+        let packs = check_trees(be, &index_be, trees, pb)?;
 
         if self.read_data {
+            let packs = index_be
+                .into_index()
+                .into_iter()
+                .filter(|p| packs.contains(&p.id));
+
+            let packs = self.read_data_subset.apply(packs);
+
+            repo.warm_up_wait(packs.iter().map(|pack| pack.id))?;
+
+            let total_pack_size = packs.iter().map(|pack| u64::from(pack.pack_size())).sum();
             let p = pb.progress_bytes("reading pack data...");
             p.set_length(total_pack_size);
 
-            index_be
-                .into_index()
-                .into_iter()
-                .par_bridge()
-                .for_each(|pack| {
-                    let id = pack.id;
-                    let data = be.read_full(FileType::Pack, &id).unwrap();
-                    match check_pack(be, pack, data, &p) {
-                        Ok(()) => {}
-                        Err(err) => error!("Error reading pack {id} : {err}",),
-                    }
-                });
+            packs.into_par_iter().for_each(|pack| {
+                let id = pack.id;
+                let data = be.read_full(FileType::Pack, &id).unwrap();
+                match check_pack(be, pack, data, &p) {
+                    Ok(()) => {}
+                    Err(err) => error!("Error reading pack {id} : {err}",),
+                }
+            });
             p.finish();
         }
         Ok(())
@@ -375,19 +469,13 @@ fn check_packs_list(be: &impl ReadBackend, mut packs: HashMap<PackId, u32>) -> R
 /// # Errors
 ///
 /// If a snapshot or tree is missing or has a different size
-fn check_snapshots(
+fn check_trees(
     be: &impl DecryptReadBackend,
     index: &impl ReadGlobalIndex,
+    snap_trees: Vec<TreeId>,
     pb: &impl ProgressBars,
-) -> RusticResult<()> {
-    let p = pb.progress_counter("reading snapshots...");
-    let snap_trees: Vec<_> = be
-        .stream_all::<SnapshotFile>(&p)?
-        .iter()
-        .map_ok(|(_, snap)| snap.tree)
-        .try_collect()?;
-    p.finish();
-
+) -> RusticResult<BTreeSet<PackId>> {
+    let mut packs = BTreeSet::new();
     let p = pb.progress_counter("checking trees...");
     let mut tree_streamer = TreeStreamerOnce::new(be, index, snap_trees, p)?;
     while let Some(item) = tree_streamer.next().transpose()? {
@@ -404,12 +492,17 @@ fn check_snapshots(
                                 error!("file {:?} blob {} has null ID", path.join(node.name()), i);
                             }
 
-                            if !index.has_data(id) {
-                                error!(
-                                    "file {:?} blob {} is missing in index",
-                                    path.join(node.name()),
-                                    id
-                                );
+                            match index.get_data(id) {
+                                None => {
+                                    error!(
+                                        "file {:?} blob {} is missing in index",
+                                        path.join(node.name()),
+                                        id
+                                    );
+                                }
+                                Some(entry) => {
+                                    _ = packs.insert(entry.pack);
+                                }
                             }
                         }
                     },
@@ -423,7 +516,18 @@ fn check_snapshots(
                         Some(tree) if tree.is_null() => {
                             error!("dir {:?} subtree has null ID", path.join(node.name()));
                         }
-                        _ => {} // subtree is ok
+                        Some(id) => match index.get_tree(&id) {
+                            None => {
+                                error!(
+                                    "dir {:?} subtree blob {} is missing in index",
+                                    path.join(node.name()),
+                                    id
+                                );
+                            }
+                            Some(entry) => {
+                                _ = packs.insert(entry.pack);
+                            }
+                        }, // subtree is ok
                     }
                 }
 
@@ -432,7 +536,7 @@ fn check_snapshots(
         }
     }
 
-    Ok(())
+    Ok(packs)
 }
 
 /// Check if a pack is valid
@@ -518,4 +622,82 @@ fn check_pack(
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use insta::assert_ron_snapshot;
+    use rand::{rngs::StdRng, Rng, SeedableRng};
+    use rstest::{fixture, rstest};
+
+    const PACK_SIZE: u32 = 100_000_000;
+
+    #[fixture]
+    fn rng() -> StdRng {
+        StdRng::seed_from_u64(5)
+    }
+    fn test_packs(rng: &mut impl Rng) -> Vec<IndexPack> {
+        (0..500)
+            .map(|_| IndexPack {
+                id: PackId::from(Id::random_from_rng(rng)),
+                blobs: Vec::new(),
+                time: None,
+                size: Some(rng.gen_range(0..PACK_SIZE)),
+            })
+            .collect()
+    }
+
+    #[rstest]
+    #[case("all")]
+    #[case("5/12")]
+    #[case("5%")]
+    #[case("250MiB")]
+    fn test_read_subset(mut rng: StdRng, #[case] s: &str) {
+        let size =
+            |packs: &[IndexPack]| -> u64 { packs.iter().map(|p| u64::from(p.pack_size())).sum() };
+
+        let test_packs = test_packs(&mut rng);
+        let total_size = size(&test_packs);
+
+        let subset: ReadSubsetOption = s.parse().unwrap();
+        let packs = subset.apply_with_rng(test_packs, &mut rng);
+        let test_size = size(&packs);
+
+        match subset {
+            ReadSubsetOption::All => assert_eq!(test_size, total_size),
+            #[allow(clippy::cast_possible_truncation)]
+            #[allow(clippy::cast_precision_loss)]
+            #[allow(clippy::cast_sign_loss)]
+            ReadSubsetOption::Percentage(s) => assert!(test_size <= (total_size as f64 * s) as u64),
+            ReadSubsetOption::Size(size) => {
+                assert!(test_size <= size && size <= test_size + u64::from(PACK_SIZE));
+            }
+            ReadSubsetOption::IdSubSet(_) => {}
+        };
+
+        let ids: Vec<_> = packs.iter().map(|pack| (pack.id, pack.size)).collect();
+        assert_ron_snapshot!(s, ids);
+    }
+
+    fn test_read_subset_n_m() {
+        let test_packs = test_packs(&mut thread_rng());
+        let mut all_packs: BTreeSet<_> = test_packs.iter().map(|pack| pack.id).collect();
+
+        let mut run_with = |s: &str| {
+            let subset: ReadSubsetOption = s.parse().unwrap();
+            let packs = subset.apply(test_packs.clone());
+            for pack in packs {
+                assert!(all_packs.remove(&pack.id));
+            }
+        };
+
+        run_with("1/5");
+        run_with("2/5");
+        run_with("3/5");
+        run_with("4/5");
+        run_with("5/5");
+
+        assert!(all_packs.is_empty());
+    }
 }

crates/core/src/commands/snapshots/rustic_core__commands__check__tests__250MiB.snap
@@ -1,0 +1,18 @@
+---
+source: crates/core/src/commands/check.rs
+expression: ids
+---
+[
+  (Id("50ec36b55a9b35de779a2757571d2e4b16f6d1ca3ff73ade120901262c2b265d"), Some(25543042)),
+  (Id("d07f392b1dfebfdf50bb10a4a7857b543cbe246f84549af8902dfde9737fd425"), Some(16489215)),
+  (Id("f879520cb3e653a0c77dcc37dcd813a71ec7d972a4a6185b13f2ed240c6e72e1"), Some(3342498)),
+  (Id("8ba2cc8cf44054f35be413f12ba83969deb01f1c44660822cee1b960d69a7526"), Some(46887261)),
+  (Id("e6bae2c9c6b9d8b8a72b45590ffc6c8e034083d6a8180877f6d270537a1ac214"), Some(47685315)),
+  (Id("f77e206f69693ae3490de38ce00f5e89ae7db4808b770c60e07d64815ee0478d"), Some(9217773)),
+  (Id("20b51c8c49aff07d7063c76a863cbdcea845989ef79d4a3f8ff599687eaebe48"), Some(83677501)),
+  (Id("9630b7b1e6329e7c28eb0eeb4e0df36bbf45acf3ba5de4a0403b77e47216857a"), Some(24144078)),
+  (Id("33e053041d2de235e03cc219a8b8300d8f1e35ee034c45f4613ea782d5e672f2"), Some(2254122)),
+  (Id("7583c1099bf604771a03af7627f4122a59da07db7358484b8543e881a7939b3f"), Some(68829)),
+  (Id("a94f61701a165181c6940584ca0cd2c2355e5e1eb65a3a295fc4d1c02fa81138"), Some(2347337)),
+  (Id("5ae22a813d32049b56ac2760a5a34b8f66e30b5232f66bb8eae420c7022197e8"), Some(452910)),
+]

crates/core/src/commands/snapshots/rustic_core__commands__check__tests__5%.snap
@@ -1,0 +1,41 @@
+---
+source: crates/core/src/commands/check.rs
+expression: ids
+---
+[
+  (Id("50ec36b55a9b35de779a2757571d2e4b16f6d1ca3ff73ade120901262c2b265d"), Some(25543042)),
+  (Id("d07f392b1dfebfdf50bb10a4a7857b543cbe246f84549af8902dfde9737fd425"), Some(16489215)),
+  (Id("f879520cb3e653a0c77dcc37dcd813a71ec7d972a4a6185b13f2ed240c6e72e1"), Some(3342498)),
+  (Id("8ba2cc8cf44054f35be413f12ba83969deb01f1c44660822cee1b960d69a7526"), Some(46887261)),
+  (Id("e6bae2c9c6b9d8b8a72b45590ffc6c8e034083d6a8180877f6d270537a1ac214"), Some(47685315)),
+  (Id("f77e206f69693ae3490de38ce00f5e89ae7db4808b770c60e07d64815ee0478d"), Some(9217773)),
+  (Id("20b51c8c49aff07d7063c76a863cbdcea845989ef79d4a3f8ff599687eaebe48"), Some(83677501)),
+  (Id("e9d65ff27f350d46ab7b1147511723ad88a2453311ae209230072fa3ad7061b9"), Some(85870775)),
+  (Id("9630b7b1e6329e7c28eb0eeb4e0df36bbf45acf3ba5de4a0403b77e47216857a"), Some(24144078)),
+  (Id("80d81feb02d330b1b19847eef1799b5eef1f49d86b5868a5aac20489765690ec"), Some(22861060)),
+  (Id("4fbdeb3d00095e28cef9efae2c412ef8a22e685f90fa555eaa6bcc37b25c6508"), Some(79074939)),
+  (Id("73400ac961c6e3d938ba106167a73495e76243cc531b6487fbbb9f00c5cb2818"), Some(66656665)),
+  (Id("0aecc98eb1e9c7ab4a04a0fa3dbc86ac17f7448e05bdab4f59f2761b86ed5e9c"), Some(6459506)),
+  (Id("32fb9db957fc10ac2812e9490533757f17602dc0f514e0ef7cd4d0a19645b6b6"), Some(68515164)),
+  (Id("57e3f93b1ac0928db8a55e77824e043bcf25ee28bfed1e60aaa9d568dd79e574"), Some(93597568)),
+  (Id("1afe739c8f3703e6b2807107db592d0ef6269c43a44716e08f99311844904b87"), Some(87610524)),
+  (Id("ed49d37e5e9404c82f1373c604335b546e962b6b997fea552ce44a1ab12765a7"), Some(13510682)),
+  (Id("b7c3c37a41c4504ed46348908978eeece1732e2414d6eeed10f3b27fb07cfded"), Some(56975313)),
+  (Id("213d4565ca5161123aec22060b362fc6188e2ee41c11df09e5db868050c251bd"), Some(7883153)),
+  (Id("476746a3956f3fc56c4391c1f2a6485de2e707f97b43e6fde302d8ef58d03b14"), Some(99496649)),
+  (Id("0e5f2ece590104cea9da3366217853c49f3ebbd949721a1ba225648453a6158e"), Some(33703536)),
+  (Id("e2c2a4449c2fd35beedfa98d006f9ec2c638c41e745dc7e51c45c341abab1769"), Some(8720062)),
+  (Id("0bc1f65e245a4716b44b80f79fe66af53caf1625ec57d94ed063411f04fef7f4"), Some(98462709)),
+  (Id("adf972f778d97c5b1cd00e2fe04954af594a64e7b8b018e7e572794a0c33f47f"), Some(91729180)),
+  (Id("d2d5c26249cf84a2822c710edfe0e9f10eeed8ae74cf318e0530f6cb08c51425"), Some(8024813)),
+  (Id("cd3049b4967c028ae7459698cdc110cea7eb197c60d8a7488f2793a0f87a47d4"), Some(6894690)),
+  (Id("bac90a9cf3bcd7e7222369bbf00dc9de9c183780a0e602a6213f8c1134fb22c4"), Some(16398765)),
+  (Id("069df78d3a7f51f0171a9d64ed58c1728fc0f86fe725680d920ffb384d0068bd"), Some(8971656)),
+  (Id("b0aa5dc32c279b4a9b0c026e41fe0925b12f70a32d3bb854110c16dcf70ae9c4"), Some(20947321)),
+  (Id("33e053041d2de235e03cc219a8b8300d8f1e35ee034c45f4613ea782d5e672f2"), Some(2254122)),
+  (Id("a39e69f47f6617866beeff292c25613aa4776abe8f11108035fa38bc72030c59"), Some(4277145)),
+  (Id("7583c1099bf604771a03af7627f4122a59da07db7358484b8543e881a7939b3f"), Some(68829)),
+  (Id("5ae22a813d32049b56ac2760a5a34b8f66e30b5232f66bb8eae420c7022197e8"), Some(452910)),
+  (Id("01324e1657f32d887da2be566b060d1f53902a399498869b56f942cfbae50705"), Some(332870)),
+  (Id("3e26211a1a4e63cfaf22d5f3e4c87a0fdc521f177535620336301813d4a335b6"), Some(122381)),
+]

crates/core/src/commands/snapshots/rustic_core__commands__check__tests__5__12.snap
@@ -1,0 +1,39 @@
+---
+source: crates/core/src/commands/check.rs
+expression: ids
+---
+[
+  (Id("ed49d37e5e9404c82f1373c604335b546e962b6b997fea552ce44a1ab12765a7"), Some(13510682)),
+  (Id("05f409ef8f73a6c4b6a56d8782f79756955576a285a3c39bbd674a29adeedf90"), Some(61236940)),
+  (Id("b13349f4e2b1ff600d17d3aef70f25cc4d5ffcd8ba80e7f3c46b37151641d1c5"), Some(60477012)),
+  (Id("a16680a002905ed49dbf84851f5a4d0fd9a6ef7f783a69599b2845689fd3774d"), Some(5256686)),
+  (Id("2d02b51df763489e64ecc59c0678dfbeb808e75775d11f6f4727aaec7cd5763e"), Some(56964272)),
+  (Id("b93c546011f8f398d4bb9351bab79b91693b02208ea4b6b9860bfb8c9cc5a25e"), Some(85572176)),
+  (Id("01c12f4c2f68e9e96fe4935242fac755a0ed9d6d8f249c92ab234847d141930c"), Some(77841320)),
+  (Id("c5bc2389666a913dae6254dc0da0c2a4268778b0f2aec695e75ddefd5641f2ba"), Some(66233200)),
+  (Id("1595155af06c09834c46936d921e0e82a225dd7bc54f9c045ba7e710b569a2e1"), Some(98606704)),
+  (Id("e5b2e72d3474bec236074943d8bfafe88f16462f81629eb8faeb4d34ac06bd9f"), Some(52879925)),
+  (Id("19fdef223293ce96c81c383459731ab2d973796815733bd81880819ebca414c4"), Some(45698795)),
+  (Id("01a8cb9bb17f736297f3f2d6c870c3c63aa518180f1f65793d56e993a39031ef"), Some(71425965)),
+  (Id("cd3049b4967c028ae7459698cdc110cea7eb197c60d8a7488f2793a0f87a47d4"), Some(6894690)),
+  (Id("ed86d1d05d6693df14f32e38a692cfac5634cc5b075fa2e7206d4fff9dd93d4f"), Some(62093554)),
+  (Id("39e85ed58a1ff03a9d340d18054e70961a5af0cf39dce2857488563804456f52"), Some(88312189)),
+  (Id("ad980c1316495d55ed49233c8adeef7f31c50e3be033da73aba884c42de9a7c3"), Some(57914095)),
+  (Id("25b85442540500c1542083649b882be38d47243b2a46d1b6be9b7a4866738a56"), Some(61033087)),
+  (Id("35fb0a065bd90575a8bb3acd9b315f213bf0880a52888ef8f875847085ee3f7a"), Some(48811146)),
+  (Id("1db4e7abd8f7bac90caca9657fa131bbee769766f425a1db0ca6e9cb5b92adcc"), Some(45713686)),
+  (Id("198d4ce71d42827765fdab01f65f765469939fb7a14d172f70ec4b8927a18799"), Some(39650195)),
+  (Id("f53b922989ccc6656ac96019b18a8a11f067e97e938493549fc5f2db1f84cc84"), Some(54276772)),
+  (Id("55d2f4278ad11c8713274b81d88b60d06d625920360ffdf0e01523bdc52abb47"), Some(80911836)),
+  (Id("191104767f95246d3946f77375c17d692f55deeb8a61ca4d0483868b3aa0f807"), Some(22610310)),
+  (Id("450290b4d8be35d70831cd8e2d959c0f6ab6753f422c69781655e3702e488280"), Some(12417994)),
+  (Id("c906750bc6fe5d788237fdf61565dc1d0bc95241ff62cc5f24c719f08775e460"), Some(98166850)),
+  (Id("4d3dee0ae9d3d46263a51783802c5ce8995261d03946ea8f556273b018393436"), Some(18639141)),
+  (Id("b53d3c0f6e3a43c9cf35c68831079d4ad3043354558d52f9941a9c1dafcb86f7"), Some(91289786)),
+  (Id("49940c21180815deccecaad0f7d9f3114edcef219d889c38657ce88286309f71"), Some(53094398)),
+  (Id("2dae514a1777cf8ac5d78402af32234ee60e53bf1e69eb2214831d7b3a576829"), Some(1700873)),
+  (Id("7112f32a249a72883663a3c52a6c49b46429c5e5aae9e400e7e2d7aaba2ae828"), Some(29717536)),
+  (Id("255d2a4fbbbbd47d585e5515f3f9b60376970bbc692592d57d4827949b99537f"), Some(75776186)),
+  (Id("cd10ef439ed00c7b3ab758d35195d2ec87eb2465c7dc8efc4ecccf8bdb7aa12a"), Some(45284384)),
+  (Id("6d363e38a62a8ad3937357ae55057afd7edc5912ac840842145f3a43b07fdd75"), Some(7647470)),
+]

crates/core/src/error.rs
@@ -8,7 +8,7 @@
 use std::{
     error::Error,
     ffi::OsString,
-    num::{ParseIntError, TryFromIntError},
+    num::{ParseFloatError, ParseIntError, TryFromIntError},
     ops::RangeInclusive,
     path::{PathBuf, StripPrefixError},
     process::ExitStatus,
@@ -185,6 +185,8 @@ pub enum CommandErrorKind {
     PackNotExisting(PackId),
     /// pack {0} got no decision what to do
     NoDecision(PackId),
+    /// {0:?}
+    FromParseFloatError(#[from] ParseFloatError),
     /// {0:?}
     FromParseIntError(#[from] ParseIntError),
     /// {0}

crates/core/src/id.rs
@@ -108,10 +108,16 @@ impl Id {
 
     /// Generate a random `Id`.
     #[must_use]
-    pub fn random() -> Self {
+    pub fn random_from_rng(rng: &mut impl RngCore) -> Self {
         let mut id = Self::default();
-        thread_rng().fill_bytes(&mut id.0);
+        rng.fill_bytes(&mut id.0);
         id
+    }
+
+    /// Generate a random `Id`.
+    #[must_use]
+    pub fn random() -> Self {
+        Self::random_from_rng(&mut thread_rng())
     }
 
     /// Convert to [`HexId`].
@@ -168,6 +174,12 @@ impl Id {
         // check if SHA256 matches
         let mut vec = vec![0; length];
         r.read_exact(&mut vec).is_ok() && self == &hash(&vec)
+    }
+
+    /// returns the first 4 bytes as u32 (interpreted as little endian)
+    #[must_use]
+    pub fn as_u32(&self) -> u32 {
+        u32::from_le_bytes([self.0[0], self.0[1], self.0[2], self.0[3]])
     }
 }
 

crates/core/src/lib.rs
@@ -135,7 +135,7 @@ pub use crate::{
     },
     commands::{
         backup::{BackupOptions, ParentOptions},
-        check::CheckOptions,
+        check::{CheckOptions, ReadSubsetOption},
         config::ConfigOptions,
         copy::CopySnapshot,
         forget::{ForgetGroup, ForgetGroups, ForgetSnapshot, KeepOptions},

commit e45a427867deab1437f247fce29547c34ab819bb

fix: fix check without --read-data (#299)
Fixes a regression introduced by #262: Check without `--read-data`
throws lots of error because we changed the access to the index.

crates/core/src/commands/check.rs
@@ -185,7 +185,7 @@ impl CheckOptions {
             }
         }
 
-        let index_collector = check_packs(be, hot_be, self.read_data, pb)?;
+        let index_collector = check_packs(be, hot_be, pb)?;
 
         if let Some(cache) = &cache {
             let p = pb.progress_spinner("cleaning up packs from cache...");
@@ -363,16 +363,11 @@ fn check_cache_files(
 fn check_packs(
     be: &impl DecryptReadBackend,
     hot_be: &Option<impl ReadBackend>,
-    read_data: bool,
     pb: &impl ProgressBars,
 ) -> RusticResult<IndexCollector> {
     let mut packs = HashMap::new();
     let mut tree_packs = HashMap::new();
-    let mut index_collector = IndexCollector::new(if read_data {
-        IndexType::Full
-    } else {
-        IndexType::DataIds
-    });
+    let mut index_collector = IndexCollector::new(IndexType::Full);
 
     let p = pb.progress_counter("reading index...");
     for index in be.stream_all::<IndexFile>(&p)? {