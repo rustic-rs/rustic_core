db6ddef16d3f2afd841eef11a657ff2365078c4a
simonsan <14062932+simonsan@users.noreply.github.com>
2024-10-26 22:36:45 +0200

    update error
    Signed-off-by: simonsan <14062932+simonsan@users.noreply.github.com>

--- crates/core/tests/errors.rs
+++ crates/core/tests/errors.rs
@@ -1,28 +1,30 @@
 use rstest::{fixture, rstest};
-use std::backtrace::Backtrace;
 
 use rustic_core::{ErrorKind, RusticError, Severity, Status};
 
 #[fixture]
-fn error() -> RusticError {
+fn error() -> Box<RusticError> {
     RusticError::new(
         ErrorKind::Io,
         "A file could not be read, make sure the file is existing and readable by the system.",
     )
     .attach_status(Status::Permanent)
     .attach_severity(Severity::Error)
-    .attach_error_code("E001".into())
+    .attach_error_code("E001")
     .attach_context("path", "/path/to/file")
     .attach_context("called", "used s3 backend")
-    .attach_source(std::io::Error::new(std::io::ErrorKind::Other, "networking error").into())
+    .attach_source(std::io::Error::new(
+        std::io::ErrorKind::Other,
+        "networking error",
+    ))
 }
 
 #[rstest]
-fn test_error_display(error: RusticError) {
+fn test_error_display(error: Box<RusticError>) {
     insta::assert_snapshot!(error);
 }
 
 #[rstest]
-fn test_error_debug(error: RusticError) {
+fn test_error_debug(error: Box<RusticError>) {
     insta::assert_debug_snapshot!(error);
 }
