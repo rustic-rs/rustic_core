9b4f0b3c5b7ed94b5ff1dfc04f88c762356ddf68
aawsome <37850842+aawsome@users.noreply.github.com>
2025-06-29 14:27:51 +0200

    fix: improve handling of u32 conversions (#412)
    As packs are only allowed up to a size of 4GiB, some internal data
    structures use `u32` instead of `usize` or `u64` to save space. This PR
    improves the handling of errors in the needed type conversations. As we
    assume at least a 32bit systems, conversions from `u32` to `usize` will
    panic.

--- crates/core/src/crypto/hasher.rs
+++ crates/core/src/crypto/hasher.rs
@@ -1,3 +1,5 @@
+use std::io::{ErrorKind, Read, Result};
+
 use sha2::{Digest, Sha256};
 
 use crate::id::Id;
@@ -14,4 +16,53 @@ use crate::id::Id;
 #[must_use]
 pub fn hash(data: &[u8]) -> Id {
     Id::new(Sha256::digest(data).into())
+}
+
+/// Hashes the data from a [`Read`]er.
+///
+/// # Arguments
+///
+/// * `reader` - The reader to read the data to hash from.
+///
+/// # Returns
+///
+/// # Errors
+/// - if the reader encounters an error
+///
+/// The hash Id of the data.
+pub fn hash_reader(mut reader: impl Read) -> Result<Id> {
+    let mut buffer = [0; 4096];
+    let mut hasher = Sha256::default();
+
+    loop {
+        match reader.read(&mut buffer) {
+            Err(err) => {
+                if err.kind() != ErrorKind::Interrupted {
+                    break Err(err);
+                }
+            }
+            Ok(count) => {
+                if count == 0 {
+                    let id = hasher.finalize();
+                    break Ok(Id::new(id.into()));
+                }
+                hasher.update(&buffer[..count]);
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use proptest::prelude::*;
+
+    proptest! {
+        #[test]
+        fn hash_reader_is_identical_to_hash(bytes in prop::collection::vec(prop::num::u8::ANY, 0..65536))  {
+            let hash1 = hash(&bytes);
+            let hash2 = hash_reader(&*bytes).unwrap();
+            prop_assert_eq!(hash1, hash2);
+        }
+    }
 }
