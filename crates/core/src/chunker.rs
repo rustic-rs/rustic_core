3895464d33e4f0085afaa63c1ea0c42ad4e04c60
Alexander Weiss <alex@weissfam.de>
2025-08-11 14:32:26 +0200

    implement FixedSize chunking

--- crates/core/src/chunker.rs
+++ crates/core/src/chunker.rs
@@ -1,15 +1,21 @@
 use std::io::Read;
 
+mod fixed_size;
 pub mod rabin;
 
+use fixed_size::ChunkIter as FixedSizeChunkIter;
 use rabin::ChunkIter as RabinChunkIter;
 use rustic_cdc::Rabin64;
 
-use crate::{RusticResult, repofile::ConfigFile};
+use crate::{
+    RusticResult,
+    repofile::{ConfigFile, configfile::Chunker},
+};
 
 /// `ChunkIter` is an iterator that chunks data.
 pub(crate) enum ChunkIter<R: Read + Send> {
     Rabin(Box<RabinChunkIter<R>>),
+    FixedSize(FixedSizeChunkIter<R>),
 }
 
 impl<R: Read + Send> ChunkIter<R> {
@@ -18,16 +24,25 @@ impl<R: Read + Send> ChunkIter<R> {
         reader: R,
         size_hint: usize,
     ) -> RusticResult<Self> {
-        let poly = config.poly()?;
-        let rabin = Rabin64::new_with_polynom(6, &poly);
-        let iter = Self::Rabin(Box::new(RabinChunkIter::new(
-            rabin,
-            config.chunk_size(),
-            config.chunk_min_size(),
-            config.chunk_max_size(),
-            reader,
-            size_hint,
-        )?));
+        let iter = match config.chunker() {
+            Chunker::Rabin => {
+                let poly = config.poly()?;
+                let rabin = Rabin64::new_with_polynom(6, &poly);
+                Self::Rabin(Box::new(RabinChunkIter::new(
+                    rabin,
+                    config.chunk_size(),
+                    config.chunk_min_size(),
+                    config.chunk_max_size(),
+                    reader,
+                    size_hint,
+                )?))
+            }
+            Chunker::FixedSize => Self::FixedSize(FixedSizeChunkIter::new(
+                config.chunk_size(),
+                reader,
+                size_hint,
+            )),
+        };
         Ok(iter)
     }
 }
@@ -38,6 +53,7 @@ impl<R: Read + Send> Iterator for ChunkIter<R> {
     fn next(&mut self) -> Option<Self::Item> {
         match self {
             Self::Rabin(rabin) => rabin.next(),
+            Self::FixedSize(fixed_size) => fixed_size.next(),
         }
     }
 }
